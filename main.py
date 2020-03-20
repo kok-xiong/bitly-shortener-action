8b234f5ed4b919c0ec84f6a86e9966754d676158
kok-xiong <kok-xiong@users.noreply.github.com>
2020-03-20 22:21:03 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import os
 import requests  # noqa We are just importing this to prove the dependency installed correctly
+import time
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+    API_KEY = os.environ["INPUT_BITLYTOKEN"]
+    LONG_URL = os.environ["INPUT_LONGURL"]
 
-    my_output = f"Hello {my_input}"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + API_KEY}
+    data = {"long_url": LONG_URL}
+    API_ENDPOINT = "https://api-ssl.bitly.com/v4/shorten"
 
-    print(f"::set-output name=myOutput::{my_output}")
+    # Request for bitly shortened link
+    r = None
+    r = requests.post(url=API_ENDPOINT, headers=headers, json=data)
+    while True:
+        time.sleep(1)
+        if r is not None:
+            break
+    if(r.status_code == 200 or r.status_code == 201):
+        shortened_link = r.json()["link"]
+    else:
+        shortened_link = ""
+
+    print(f"::set-output name=shortURL::{shortened_link}")
+
+    output = "Hello world"
+    print(f"::set-output name=myOutput::{output}")
 
 
 if __name__ == "__main__":
